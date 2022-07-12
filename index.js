e92f4fbaf58f65ad32634514e8175c8339698188
jaluik <cqzhengmouren@gmail.com>
2022-07-12 22:35:42 +0800

    fix: 修复替换不完全的问题

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const fs = require('fs');
+const fs = require("fs");
 
-const PluginName = 'RemoveUnusedWxPlugin';
+const PluginName = "RemoveUnusedWxPlugin";
 
 class RemoveUnusedWxPlugin {
   constructor(opts) {
@@ -9,26 +9,26 @@ class RemoveUnusedWxPlugin {
 
   apply(compiler) {
     const testReg = /\.js$/;
-    compiler.hooks.done.tap(PluginName, stat => {
+    compiler.hooks.done.tap(PluginName, (stat) => {
       const assets = stat.compilation.assets;
-      Object.keys(assets).map(fileName => {
+      Object.keys(assets).map((fileName) => {
         const fileObj = assets[fileName];
         if (testReg.test(fileName)) {
           const filepath = fileObj.existsAt;
           if (fs.existsSync(filepath)) {
-            fs.readFile(filepath, 'utf-8', (err, data) => {
+            fs.readFile(filepath, "utf-8", (err, data) => {
               if (err) {
                 console.log(err);
               }
               let result = data;
-              this.opts.filterList.forEach(i => {
-                result = result.replace(`wx.${i}`, 'undefined');
+              this.opts.filterList.forEach((i) => {
+                result = result.replaceAll(`wx.${i}`, "undefined");
               });
               if (result !== data) {
-                console.log('处理的文件：', fileName);
-                fs.writeFile(filepath, result, err => {
+                console.log("处理的文件：", fileName);
+                fs.writeFile(filepath, result, (err) => {
                   if (err) {
-                    console.log('err', err);
+                    console.log("err", err);
                   }
                 });
               }
